08867cdbf0c86f6c8ee203b901c9e97d3e2ff512
bryanlesmart <bryansilmaro03@gmail.com>
2024-07-09 20:57:01 +0800

    code the start pack and add feature for wayland

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use ball::Ball;
+
+mod ball;
+
+const SCREEN_WIDTH: f32 = 800.0;
+const SCREEN_HEIGTH: f32 = 600.0;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, t) = raylib::init()
+        .size(SCREEN_WIDTH as i32, SCREEN_HEIGTH as i32)
+        .title("ball")
+        .build();
+
+    let mut ball = Ball::new();
+
+    rl.set_target_fps(60);
+
+    while !rl.window_should_close() {
+        ball.update(&rl);
+        ball.draw(&mut rl, &t);
+    }
 }
