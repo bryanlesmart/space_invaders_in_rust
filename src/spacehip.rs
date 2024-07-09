206187d7308f33e7bca2c990f48e7154e3f47ef6
bryanlesmart <bryansilmaro03@gmail.com>
2024-07-09 21:45:43 +0800

    v1 spacehip keyinput and collision in screen width

--- src/spacehip.rs
+++ src/spacehip.rs
@@ -1,3 +1,4 @@
+use raylib::consts::KeyboardKey::*;
 use raylib::prelude::*;
 
 pub struct Spacehip {
@@ -7,17 +8,29 @@ pub struct Spacehip {
 
 impl Spacehip {
     pub fn new(rl: &mut RaylibHandle, filename: &'static str, t: &RaylibThread) -> Self {
+        let sprite = rl.load_texture(&t, filename).unwrap();
         return Spacehip {
-            position: Vector2::zero(),
-            sprite: rl.load_texture(&t, filename).unwrap(),
+            position: Vector2::new(
+                (rl.get_screen_width() as f32 - sprite.width() as f32) / 2.0,
+                rl.get_screen_height() as f32 - sprite.height() as f32,
+            ),
+            sprite,
         };
     }
 
     pub fn spaceship_update(&mut self, rl: &RaylibHandle) {
-        self.position = Vector2::new(
-            rl.get_screen_width() as f32 / 2.0,
-            rl.get_screen_height() as f32 / 2.0,
-        );
+        if rl.is_key_down(KEY_LEFT) {
+            self.position.x -= 7.0;
+        } else if rl.is_key_down(KEY_RIGHT) {
+            self.position.x += 7.0;
+        }
+
+        //Collision to with of screen
+        if self.position.x >= rl.get_screen_width() as f32 - self.sprite.width() as f32 {
+            self.position.x = rl.get_screen_width() as f32 - self.sprite.width() as f32;
+        } else if self.position.x < 0.0 {
+            self.position.x = 0.0;
+        }
     }
 
     pub fn spaceship_draw(&self, d: &mut RaylibDrawHandle) {
